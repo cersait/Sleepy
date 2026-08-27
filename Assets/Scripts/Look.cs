2728e496646da6a2982a3835aa84550561535766
cersait <atillaesen162@gmail.com>
2026-08-27 10:39:22 +0200

    Guy yes probuld

---
+++ Assets/Scripts/Look.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    public float mouseSen = 100f;
+    public Transform player;
+    float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Låser vår mus till skärmen, och så den inte syns. 
+    }
+    void Update()
+    {
+        //Hämtar vår axis från input manager under project settings
+        float mouseX = Input.GetAxis("Mouse X") * mouseSen * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSen * Time.deltaTime;
+        xRotation -= mouseY;
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Vi använder rotate för att vi ska kunna stoppa rotationen från att gå förlångt / snurra ett helt varv
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
